cecba43da5c0fa0ee1dff5f0165b7098127b5a3d
NotFour <NotFour@yandex.ru>
2017-05-02 10:23:53 +0300

    add google-map using js, disabled scroll

--- js/init.js
+++ js/init.js
@@ -1,10 +1,13 @@
-var uluru = {lat: 57.048671, lng: 9.9201409};
-var map = new google.maps.Map(document.getElementsByClassName('google-map')[0], {
-    zoom: 17,
-    scrollwheel: false,
-    center: uluru,
-});
-var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-});
+function initMap(){
+    var uluru = {lat: 57.048671, lng: 9.9201409};
+    var map = new google.maps.Map(document.getElementsByClassName('google-map')[0], {
+        zoom: 17,
+        scrollwheel: false,
+        center: uluru,
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
+initMap();
--- js/script.js
+++ js/script.js
@@ -2,23 +2,23 @@ var toggle = document.getElementsByClassName('menu__toggle')[0];
 var menu = document.getElementsByClassName('menu')[0];
 var menuList = document.getElementsByClassName('menu__list')[0];
 var toggleInner = document.getElementsByClassName('menu__toggle-label')[0];
-var html = document.getElementsByName('html');
+var html = document.getElementsByTagName('html')[0];
 toggle.onclick = function () {
     if (menu.classList.contains('menu_active')) {
         menuList.classList.remove('menu__list_active');
         toggleInner.classList.remove('menu__toggle-label_active');
         this.classList.remove('menu__toggle_active');
         menu.classList.remove('menu_active');
-        //html.setAttribute('style', 'overflow:hidden');
+        html.removeAttribute('style');
     } else {
         menu.classList.add('menu_active');
         this.classList.add('menu__toggle_active');
         toggleInner.classList.add('menu__toggle-label_active');
         menuList.classList.add('menu__list_active');
-       // html.removeAttribute('style');
+        html.setAttribute('style', 'overflow:hidden');
     }
 };
-
+/*
 var menuLink = document.getElementsByClassName('menu__list-link');
 for (var i = 0; i < menuLink.size; i++) {
     menuLink[i].onclick = function () {
@@ -30,6 +30,4 @@ for (var i = 0; i < menuLink.size; i++) {
             menu.classList.remove('menu_active');
         }
     }
-}
-
-//AIzaSyA7vAfPUnLBpuLbxw41HSuM4zsAGOKAcvw мой ключ апи  var uluru = {lat: 57.048671, lng: 9.9201409};
+}*/
